16d69eebed6fb0533d852a8d09edb4ec19bf9cca
RaniaG <raniagml12@gmail.com>
2019-04-28 00:22:01 +0200

    fixed bug in admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const AdminModel = require('../models/admin');
+const createError = require('http-errors');
 
 router.post('/', async function (req, res, next) {
     debugger;
